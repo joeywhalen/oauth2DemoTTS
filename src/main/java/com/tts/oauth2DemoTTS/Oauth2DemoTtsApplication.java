adc2cc6616ecee1637ebcbee7f7bd0baef8532cb
Joey Whalen <jtktwhalen@gmail.com>
2021-02-24 16:18:21 -0500

    initial commit

---
+++ src/main/java/com/tts/oauth2DemoTTS/Oauth2DemoTtsApplication.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2DemoTTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2DemoTtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2DemoTtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/oauth2DemoTTS/Oauth2DemoTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2DemoTTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2DemoTtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
